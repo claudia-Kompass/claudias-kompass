6d6991c4ad772bdaab629dd4a1c47d920030479e
claudia-Kompass <claudia.foerstner@googlemail.com>
2026-02-22 02:54:29 +0100

    Update daily.js

--- api/daily.js
+++ api/daily.js
@@ -1,4 +1,5 @@
 export default async function handler(req, res) {
+  res.setHeader("Cache-Control", "no-store");
   if (req.method !== "GET") {
     return res.status(405).json({ error: "Method not allowed" });
   }
