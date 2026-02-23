674f2d04ae936c412b7fecaf2005930e9c8cd57e
claudia-Kompass <claudia.foerstner@googlemail.com>
2026-02-23 16:07:01 +0100

    Update daily.js

--- api/daily.js
+++ api/daily.js
@@ -1,22 +1,18 @@
 import { getExecutive } from "../lib/executive.js";
+import { getRegional } from "../lib/regional.js";
 
 export default async function handler(req, res) {
   try {
     const executive = getExecutive();
+    const regional = getRegional();
 
     res.status(200).json({
-      version: "1.0.0",
+      version: "1.1.0",
       executive,
-      regional: "Modul folgt",
+      regional,
       weather: "Modul folgt",
       personal: "Modul folgt",
       travel: "Modul folgt"
-    });
-
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({
-      error: "Daily-Kompass Fehler"
     });
   }
 }
