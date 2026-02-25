1f09567a469cf0c8071ce5414363361363b1cc4a
claudia-Kompass <claudia.foerstner@googlemail.com>
2026-02-25 19:19:40 +0100

    Update daily.js

--- api/daily.js
+++ api/daily.js
@@ -93,7 +93,7 @@ export default async function handler(req, res) {
 
     return res.status(200).json({
       version,
-      timestamp,
+      marketTime: timestamp,
       weather,
       crypto
     });
