a771ad02dd300be934857e6af2860b503d54be06
claudia-Kompass <claudia.foerstner@googlemail.com>
2026-03-07 20:37:42 +0100

    add ukulele songs

--- api/data/ukulele.js
+++ api/data/ukulele.js
@@ -1,1 +1,43 @@
+module.exports = [
 
+{
+title:"Somewhere Over The Rainbow",
+artist:"Israel Kamakawiwo'ole",
+chords:"C G Am F",
+strumming:"D-DU-UDU",
+tempo:"Slow"
+},
+
+{
+title:"Riptide",
+artist:"Vance Joy",
+chords:"Am G C",
+strumming:"D-DU-UDU",
+tempo:"Medium"
+},
+
+{
+title:"Stand By Me",
+artist:"Ben E. King",
+chords:"C Am F G",
+strumming:"D-DU-UDU",
+tempo:"Medium"
+},
+
+{
+title:"I'm Yours",
+artist:"Jason Mraz",
+chords:"C G Am F",
+strumming:"D-DU-UDU",
+tempo:"Medium"
+},
+
+{
+title:"Let It Be",
+artist:"The Beatles",
+chords:"C G Am F",
+strumming:"Down Down Up Up Down Up",
+tempo:"Slow"
+}
+
+]
