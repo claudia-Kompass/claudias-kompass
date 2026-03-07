82fe2776df95698b1de4cc97f817bf61e94bd988
claudia-Kompass <claudia.foerstner@googlemail.com>
2026-03-07 20:56:40 +0100

    Update ukulele.js

--- api/data/ukulele.js
+++ api/data/ukulele.js
@@ -3,10 +3,7 @@ module.exports = [
 {
 title:"Pacific Light",
 chords:["G","Em","C","D"],
-tab:`A|--2-|--2-|--3-|--0-|
-E|--3-|--3-|--0-|--2-|
-C|--2-|--4-|--0-|--2-|
-G|--0-|--0-|--0-|--2-|`,
+tab:"A|--2-|--2-|--3-|--0-|\nE|--3-|--3-|--0-|--2-|\nC|--2-|--4-|--0-|--2-|\nG|--0-|--0-|--0-|--2-|",
 tempo:"80",
 strum:"Down Down Up Up Down",
 picking:"Thumb Index Thumb"
@@ -15,10 +12,7 @@ picking:"Thumb Index Thumb"
 {
 title:"Somewhere Over The Rainbow",
 chords:["C","G","Am","F"],
-tab:`A|--3-|--2-|--0-|--0-|
-E|--0-|--3-|--0-|--1-|
-C|--0-|--2-|--0-|--0-|
-G|--0-|--0-|--2-|--2-|`,
+tab:"A|--3-|--2-|--0-|--0-|\nE|--0-|--3-|--0-|--1-|\nC|--0-|--2-|--0-|--0-|\nG|--0-|--0-|--2-|--2-|",
 tempo:"75",
 strum:"Down Down Up Up Down Up",
 picking:"Thumb Index Middle"
@@ -27,10 +21,7 @@ picking:"Thumb Index Middle"
 {
 title:"Riptide",
 chords:["Am","G","C"],
-tab:`A|--0-|--2-|--3-|
-E|--0-|--3-|--0-|
-C|--0-|--2-|--0-|
-G|--2-|--0-|--0-|`,
+tab:"A|--0-|--2-|--3-|\nE|--0-|--3-|--0-|\nC|--0-|--2-|--0-|\nG|--2-|--0-|--0-|",
 tempo:"100",
 strum:"Down Down Up Down Up",
 picking:"Thumb Index"
@@ -39,10 +30,7 @@ picking:"Thumb Index"
 {
 title:"Stand By Me",
 chords:["C","Am","F","G"],
-tab:`A|--3-|--0-|--0-|--2-|
-E|--0-|--0-|--1-|--3-|
-C|--0-|--0-|--0-|--2-|
-G|--0-|--2-|--2-|--0-|`,
+tab:"A|--3-|--0-|--0-|--2-|\nE|--0-|--0-|--1-|--3-|\nC|--0-|--0-|--0-|--2-|\nG|--0-|--2-|--2-|--0-|",
 tempo:"85",
 strum:"Down Down Up Up Down Up",
 picking:"Thumb Index Middle"
@@ -51,10 +39,7 @@ picking:"Thumb Index Middle"
 {
 title:"Let It Be",
 chords:["C","G","Am","F"],
-tab:`A|--3-|--2-|--0-|--0-|
-E|--0-|--3-|--0-|--1-|
-C|--0-|--2-|--0-|--0-|
-G|--0-|--0-|--2-|--2-|`,
+tab:"A|--3-|--2-|--0-|--0-|\nE|--0-|--3-|--0-|--1-|\nC|--0-|--2-|--0-|--0-|\nG|--0-|--0-|--2-|--2-|",
 tempo:"72",
 strum:"Down Down Up Up Down Up",
 picking:"Thumb Index Middle"
@@ -63,10 +48,7 @@ picking:"Thumb Index Middle"
 {
 title:"I'm Yours",
 chords:["C","G","Am","F"],
-tab:`A|--3-|--2-|--0-|--0-|
-E|--0-|--3-|--0-|--1-|
-C|--0-|--2-|--0-|--0-|
-G|--0-|--0-|--2-|--2-|`,
+tab:"A|--3-|--2-|--0-|--0-|\nE|--0-|--3-|--0-|--1-|\nC|--0-|--2-|--0-|--0-|\nG|--0-|--0-|--2-|--2-|",
 tempo:"95",
 strum:"Down Down Up Down Up",
 picking:"Thumb Index"
@@ -75,10 +57,7 @@ picking:"Thumb Index"
 {
 title:"Three Little Birds",
 chords:["A","D","E"],
-tab:`A|--0-|--0-|--2-|
-E|--0-|--2-|--4-|
-C|--1-|--2-|--4-|
-G|--2-|--2-|--4-|`,
+tab:"A|--0-|--0-|--2-|\nE|--0-|--2-|--4-|\nC|--1-|--2-|--4-|\nG|--2-|--2-|--4-|",
 tempo:"90",
 strum:"Down Up Down Up",
 picking:"Thumb Index"
@@ -87,10 +66,7 @@ picking:"Thumb Index"
 {
 title:"La Bamba",
 chords:["C","F","G"],
-tab:`A|--3-|--0-|--2-|
-E|--0-|--1-|--3-|
-C|--0-|--0-|--2-|
-G|--0-|--2-|--0-|`,
+tab:"A|--3-|--0-|--2-|\nE|--0-|--1-|--3-|\nC|--0-|--0-|--2-|\nG|--0-|--2-|--0-|",
 tempo:"110",
 strum:"Down Down Up Down Up",
 picking:"Thumb Index"
@@ -99,10 +75,7 @@ picking:"Thumb Index"
 {
 title:"Hallelujah",
 chords:["C","Am","F","G"],
-tab:`A|--3-|--0-|--0-|--2-|
-E|--0-|--0-|--1-|--3-|
-C|--0-|--0-|--0-|--2-|
-G|--0-|--2-|--2-|--0-|`,
+tab:"A|--3-|--0-|--0-|--2-|\nE|--0-|--0-|--1-|--3-|\nC|--0-|--0-|--0-|--2-|\nG|--0-|--2-|--2-|--0-|",
 tempo:"70",
 strum:"Down Down Up Up Down Up",
 picking:"Thumb Index Middle"
@@ -111,10 +84,7 @@ picking:"Thumb Index Middle"
 {
 title:"No Woman No Cry",
 chords:["C","G","Am","F"],
-tab:`A|--3-|--2-|--0-|--0-|
-E|--0-|--3-|--0-|--1-|
-C|--0-|--2-|--0-|--0-|
-G|--0-|--0-|--2-|--2-|`,
+tab:"A|--3-|--2-|--0-|--0-|\nE|--0-|--3-|--0-|--1-|\nC|--0-|--2-|--0-|--0-|\nG|--0-|--0-|--2-|--2-|",
 tempo:"80",
 strum:"Down Down Up Down Up",
 picking:"Thumb Index"
