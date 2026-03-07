f0c24ae35e9316b0eecc92eb32079306de8aafca
claudia-Kompass <claudia.foerstner@googlemail.com>
2026-03-07 23:45:39 +0100

    Create languages.js

---
+++ api/data/languages.js
@@ -0,0 +1,63 @@
+module.exports = [
+
+{
+en:"Where is the bus stop?",
+es:"¿Dónde está la parada de autobús?",
+de:"Wo ist die Bushaltestelle?"
+},
+
+{
+en:"Two coffees please",
+es:"Dos cafés por favor",
+de:"Zwei Kaffee bitte"
+},
+
+{
+en:"How much does this cost?",
+es:"¿Cuánto cuesta esto?",
+de:"Wie viel kostet das?"
+},
+
+{
+en:"Where is the restroom?",
+es:"¿Dónde está el baño?",
+de:"Wo ist die Toilette?"
+},
+
+{
+en:"I would like a coffee.",
+es:"Quiero un café.",
+de:"Ich hätte gern einen Kaffee."
+},
+
+{
+en:"Do you speak English?",
+es:"¿Habla inglés?",
+de:"Sprechen Sie Englisch?"
+},
+
+{
+en:"Can I pay by card?",
+es:"¿Puedo pagar con tarjeta?",
+de:"Kann ich mit Karte bezahlen?"
+},
+
+{
+en:"Where is the train station?",
+es:"¿Dónde está la estación?",
+de:"Wo ist der Bahnhof?"
+},
+
+{
+en:"One moment please.",
+es:"Un momento por favor.",
+de:"Einen Moment bitte."
+},
+
+{
+en:"See you tomorrow.",
+es:"Hasta mañana.",
+de:"Bis morgen."
+}
+
+]
