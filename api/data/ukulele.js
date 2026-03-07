9e8551be8b92e439e39bc49ee4b1551bd25f399e
claudia-Kompass <claudia.foerstner@googlemail.com>
2026-03-07 21:34:50 +0100

    Update ukulele.js

--- api/data/ukulele.js
+++ api/data/ukulele.js
@@ -4,8 +4,7 @@ module.exports = [
 title:"Ukulele Test",
 tempo:80,
 strum:"Down Up",
-picking:"Thumb Index",
-
+picking:"G C E A",
 tabs:`C     G
 
 
@@ -13,6 +12,62 @@ A|--3-|--2-|
 E|--0-|--3-|
 C|--0-|--2-|
 G|--0-|--0-|`
+},
+
+{
+title:"Let It Be",
+tempo:72,
+strum:"Down Down Up Up Down Up",
+picking:"G C E A",
+tabs:`C     G     Am     F
+
+
+A|--3-|--2-|--0-|--0-|
+E|--0-|--3-|--0-|--1-|
+C|--0-|--2-|--0-|--0-|
+G|--0-|--0-|--2-|--2-|`
+},
+
+{
+title:"Riptide",
+tempo:100,
+strum:"Down Down Up Down Up",
+picking:"G C E A",
+tabs:`Am     G     C
+
+
+A|--0-|--2-|--3-|
+E|--0-|--3-|--0-|
+C|--0-|--2-|--0-|
+G|--2-|--0-|--0-|`
+},
+
+{
+title:"Stand By Me",
+tempo:85,
+strum:"Down Down Up Up Down Up",
+picking:"G C E A",
+tabs:`C     Am     F     G
+
+
+A|--3-|--0-|--0-|--2-|
+E|--0-|--0-|--1-|--3-|
+C|--0-|--0-|--0-|--2-|
+G|--0-|--2-|--2-|--0-|`
+},
+
+{
+title:"Somewhere Over The Rainbow",
+tempo:75,
+strum:"Down Down Up Up Down Up",
+picking:"G C E A",
+tabs:`C     G     Am     F
+
+
+A|--3-|--2-|--0-|--0-|
+E|--0-|--3-|--0-|--1-|
+C|--0-|--2-|--0-|--0-|
+G|--0-|--0-|--2-|--2-|`
 }
 
 ]
