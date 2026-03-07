e4c6c541fb0f1fffa26563f1e67914d802e99cd0
claudia-Kompass <claudia.foerstner@googlemail.com>
2026-03-07 21:59:00 +0100

    Create quotes.js

---
+++ api/data/quotes.js
@@ -0,0 +1,53 @@
+module.exports = [
+
+{
+text:"Der Weg entsteht beim Gehen.",
+author:"Franz Kafka"
+},
+
+{
+text:"Reisen bedeutet zu leben.",
+author:"Hans Christian Andersen"
+},
+
+{
+text:"Nicht der Wind bestimmt die Richtung, sondern die Segel.",
+author:"Sprichwort"
+},
+
+{
+text:"Mut steht am Anfang des Handelns, Glück am Ende.",
+author:"Demokrit"
+},
+
+{
+text:"Wer immer tut, was er schon kann, bleibt immer das, was er schon ist.",
+author:"Henry Ford"
+},
+
+{
+text:"Man entdeckt keine neuen Erdteile ohne den Mut, alte Küsten aus den Augen zu verlieren.",
+author:"André Gide"
+},
+
+{
+text:"Der beste Weg, die Zukunft vorherzusagen, ist sie zu gestalten.",
+author:"Peter Drucker"
+},
+
+{
+text:"Die größte Entdeckung meiner Generation ist, dass der Mensch sein Leben ändern kann.",
+author:"William James"
+},
+
+{
+text:"Der Sinn des Lebens besteht nicht darin, ein erfolgreicher Mensch zu sein, sondern ein wertvoller.",
+author:"Albert Einstein"
+},
+
+{
+text:"Das Leben beginnt dort, wo die Komfortzone endet.",
+author:"Neale Donald Walsch"
+}
+
+]
