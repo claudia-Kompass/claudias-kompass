eb4bd91e17bef84c0963ed965ade5bfddf16d6ca
claudia-Kompass <claudia.foerstner@googlemail.com>
2026-03-07 22:36:14 +0100

    create recipe database

---
+++ api/data/recipes.js
@@ -0,0 +1,72 @@
+module.exports = [
+
+{
+title:"Airfryer Gemüse Mix",
+ingredients:[
+"Paprika",
+"Zucchini",
+"Olivenöl",
+"Salz",
+"Pfeffer"
+],
+description:"Buntes Ofengemüse knusprig aus dem Airfryer.",
+temp:"180°C",
+time:"12 Minuten",
+portion:"2"
+},
+
+{
+title:"Airfryer Kartoffelspalten",
+ingredients:[
+"Kartoffeln",
+"Olivenöl",
+"Paprikapulver",
+"Salz"
+],
+description:"Knusprige Kartoffelspalten ohne viel Fett.",
+temp:"200°C",
+time:"15 Minuten",
+portion:"2"
+},
+
+{
+title:"Airfryer Lachsfilet",
+ingredients:[
+"Lachsfilet",
+"Zitrone",
+"Olivenöl",
+"Pfeffer"
+],
+description:"Saftiger Lachs mit leichter Zitronennote.",
+temp:"180°C",
+time:"10 Minuten",
+portion:"2"
+},
+
+{
+title:"Airfryer Hähnchenstreifen",
+ingredients:[
+"Hähnchenbrust",
+"Olivenöl",
+"Paprika",
+"Knoblauchpulver"
+],
+description:"Würzige Hähnchenstreifen perfekt für Salate.",
+temp:"190°C",
+time:"12 Minuten",
+portion:"2"
+},
+
+{
+title:"Airfryer Apfelchips",
+ingredients:[
+"Äpfel",
+"Zimt"
+],
+description:"Süßer Snack aus dünnen Apfelscheiben.",
+temp:"160°C",
+time:"15 Minuten",
+portion:"2"
+}
+
+]
