931dedd93c3709a279d5323f513703f8db0d0aff
claudia-Kompass <claudia.foerstner@googlemail.com>
2026-03-07 20:50:20 +0100

    Update ukulele.js

--- api/data/ukulele.js
+++ api/data/ukulele.js
@@ -1,43 +1,123 @@
 module.exports = [
 
 {
+title:"Pacific Light",
+chords:["G","Em","C","D"],
+tab:`A|--2-|--2-|--3-|--0-|
+E|--3-|--3-|--0-|--2-|
+C|--2-|--4-|--0-|--2-|
+G|--0-|--0-|--0-|--2-|`,
+tempo:"80",
+strum:"Down Down Up Up Down",
+picking:"Thumb Index Thumb"
+},
+
+{
 title:"Somewhere Over The Rainbow",
-artist:"Israel Kamakawiwo'ole",
-chords:"C G Am F",
-strumming:"D-DU-UDU",
-tempo:"Slow"
+chords:["C","G","Am","F"],
+tab:`A|--3-|--2-|--0-|--0-|
+E|--0-|--3-|--0-|--1-|
+C|--0-|--2-|--0-|--0-|
+G|--0-|--0-|--2-|--2-|`,
+tempo:"75",
+strum:"Down Down Up Up Down Up",
+picking:"Thumb Index Middle"
 },
 
 {
 title:"Riptide",
-artist:"Vance Joy",
-chords:"Am G C",
-strumming:"D-DU-UDU",
-tempo:"Medium"
+chords:["Am","G","C"],
+tab:`A|--0-|--2-|--3-|
+E|--0-|--3-|--0-|
+C|--0-|--2-|--0-|
+G|--2-|--0-|--0-|`,
+tempo:"100",
+strum:"Down Down Up Down Up",
+picking:"Thumb Index"
 },
 
 {
 title:"Stand By Me",
-artist:"Ben E. King",
-chords:"C Am F G",
-strumming:"D-DU-UDU",
-tempo:"Medium"
-},
-
-{
-title:"I'm Yours",
-artist:"Jason Mraz",
-chords:"C G Am F",
-strumming:"D-DU-UDU",
-tempo:"Medium"
+chords:["C","Am","F","G"],
+tab:`A|--3-|--0-|--0-|--2-|
+E|--0-|--0-|--1-|--3-|
+C|--0-|--0-|--0-|--2-|
+G|--0-|--2-|--2-|--0-|`,
+tempo:"85",
+strum:"Down Down Up Up Down Up",
+picking:"Thumb Index Middle"
 },
 
 {
 title:"Let It Be",
-artist:"The Beatles",
-chords:"C G Am F",
-strumming:"Down Down Up Up Down Up",
-tempo:"Slow"
+chords:["C","G","Am","F"],
+tab:`A|--3-|--2-|--0-|--0-|
+E|--0-|--3-|--0-|--1-|
+C|--0-|--2-|--0-|--0-|
+G|--0-|--0-|--2-|--2-|`,
+tempo:"72",
+strum:"Down Down Up Up Down Up",
+picking:"Thumb Index Middle"
+},
+
+{
+title:"I'm Yours",
+chords:["C","G","Am","F"],
+tab:`A|--3-|--2-|--0-|--0-|
+E|--0-|--3-|--0-|--1-|
+C|--0-|--2-|--0-|--0-|
+G|--0-|--0-|--2-|--2-|`,
+tempo:"95",
+strum:"Down Down Up Down Up",
+picking:"Thumb Index"
+},
+
+{
+title:"Three Little Birds",
+chords:["A","D","E"],
+tab:`A|--0-|--0-|--2-|
+E|--0-|--2-|--4-|
+C|--1-|--2-|--4-|
+G|--2-|--2-|--4-|`,
+tempo:"90",
+strum:"Down Up Down Up",
+picking:"Thumb Index"
+},
+
+{
+title:"La Bamba",
+chords:["C","F","G"],
+tab:`A|--3-|--0-|--2-|
+E|--0-|--1-|--3-|
+C|--0-|--0-|--2-|
+G|--0-|--2-|--0-|`,
+tempo:"110",
+strum:"Down Down Up Down Up",
+picking:"Thumb Index"
+},
+
+{
+title:"Hallelujah",
+chords:["C","Am","F","G"],
+tab:`A|--3-|--0-|--0-|--2-|
+E|--0-|--0-|--1-|--3-|
+C|--0-|--0-|--0-|--2-|
+G|--0-|--2-|--2-|--0-|`,
+tempo:"70",
+strum:"Down Down Up Up Down Up",
+picking:"Thumb Index Middle"
+},
+
+{
+title:"No Woman No Cry",
+chords:["C","G","Am","F"],
+tab:`A|--3-|--2-|--0-|--0-|
+E|--0-|--3-|--0-|--1-|
+C|--0-|--2-|--0-|--0-|
+G|--0-|--0-|--2-|--2-|`,
+tempo:"80",
+strum:"Down Down Up Down Up",
+picking:"Thumb Index"
 }
 
 ]
