9dff0821f30f7fee3a06ccd5a3df37becdd9ccab
claudia-Kompass <claudia.foerstner@googlemail.com>
2026-03-08 22:27:04 +0100

    Create events.js

---
+++ api/data/events.js
@@ -0,0 +1,168 @@
+module.exports = [
+
+/* ======================
+   VERBRAUCHERMESSEN
+====================== */
+
+{
+title:"HVM – Hohenloher Verbrauchermesse",
+city:"Ilshofen",
+location:"Arena Hohenlohe",
+address:"Eckartshäuser Str. 50, 74532 Ilshofen",
+month:3,
+day:8,
+time:"10:00–18:00",
+url:"https://www.hohenloher-verbrauchermesse.de"
+},
+
+{
+title:"Freizeit Messe Nürnberg",
+city:"Nürnberg",
+location:"Messezentrum Nürnberg",
+address:"Messezentrum 1, 90471 Nürnberg",
+month:3,
+day:8,
+time:"09:30–18:00",
+url:"https://www.freizeitmesse.de"
+},
+
+{
+title:"Consumenta",
+city:"Nürnberg",
+location:"Messezentrum Nürnberg",
+address:"Messezentrum 1, 90471 Nürnberg",
+month:10,
+day:25,
+time:"10:00–18:00",
+url:"https://www.consumenta.de"
+},
+
+{
+title:"CMT – Urlaubsmesse",
+city:"Stuttgart",
+location:"Messe Stuttgart",
+address:"Messepiazza 1, 70629 Stuttgart",
+month:1,
+day:17,
+time:"10:00–18:00",
+url:"https://www.messe-stuttgart.de/cmt"
+},
+
+{
+title:"Genussmesse Heilbronn",
+city:"Heilbronn",
+location:"redblue Messehalle",
+address:"Wannenäckerstr 50, 74078 Heilbronn",
+month:3,
+day:7,
+time:"10:00–18:00",
+url:"https://redblue.de"
+},
+
+/* ======================
+   SCHWÄBISCH HALL
+====================== */
+
+{
+title:"Haller Frühling",
+city:"Schwäbisch Hall",
+location:"Altstadt",
+address:"Marktplatz 1, 74523 Schwäbisch Hall",
+month:4,
+day:6
+},
+
+{
+title:"Jakobimarkt",
+city:"Schwäbisch Hall",
+location:"Altstadt",
+address:"Marktplatz 1, 74523 Schwäbisch Hall",
+month:7,
+day:26
+},
+
+{
+title:"Sommernachtsfest",
+city:"Schwäbisch Hall",
+location:"Altstadt",
+address:"Marktplatz 1, 74523 Schwäbisch Hall",
+month:8,
+day:10
+},
+
+{
+title:"Kuchen & Brunnenfest",
+city:"Schwäbisch Hall",
+location:"Altstadt",
+address:"Marktplatz 1, 74523 Schwäbisch Hall",
+month:6,
+day:9
+},
+
+{
+title:"Haller Weihnachtsmarkt",
+city:"Schwäbisch Hall",
+location:"Marktplatz",
+address:"Marktplatz 1, 74523 Schwäbisch Hall",
+month:12,
+day:5
+},
+
+/* ======================
+   CRAILSHEIM
+====================== */
+
+{
+title:"Crailsheimer Volksfest",
+city:"Crailsheim",
+location:"Volksfestplatz",
+address:"Beuerlbacher Str, 74564 Crailsheim",
+month:9,
+day:19
+},
+
+{
+title:"Street Food Festival",
+city:"Crailsheim",
+location:"Innenstadt",
+address:"Marktplatz 1, 74564 Crailsheim",
+month:6,
+day:15
+},
+
+/* ======================
+   GAILDORF
+====================== */
+
+{
+title:"Gaildorfer Pferdemarkt",
+city:"Gaildorf",
+location:"Innenstadt",
+address:"Marktplatz, 74405 Gaildorf",
+month:2,
+day:10
+},
+
+/* ======================
+   DINKELSBÜHL
+====================== */
+
+{
+title:"Kinderzeche",
+city:"Dinkelsbühl",
+location:"Altstadt",
+address:"Marktplatz, 91550 Dinkelsbühl",
+month:7,
+day:20
+},
+
+{
+title:"Weihnachtsmarkt Dinkelsbühl",
+city:"Dinkelsbühl",
+location:"Altstadt",
+address:"Marktplatz, 91550 Dinkelsbühl",
+month:12,
+day:10
+}
+
+]
