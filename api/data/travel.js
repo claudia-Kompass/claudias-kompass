6b56733a4fe94993580702f977090bf7a4f4ad34
claudia-Kompass <claudia.foerstner@googlemail.com>
2026-03-15 00:41:21 +0100

    Create travel.js

---
+++ api/data/travel.js
@@ -0,0 +1,31 @@
+module.exports = [
+
+{
+title:"Segeln – Sardinien",
+text:"Kristallklares Wasser und der Maddalena-Archipel machen Sardinien zu einem der schönsten Segelreviere im Mittelmeer.",
+url:"https://de.wikipedia.org/wiki/Sardinien",
+image:"https://images.unsplash.com/900x500/?sailing,sardinia"
+},
+
+{
+title:"Salsa & Kultur – Havanna",
+text:"Koloniale Architektur, kubanische Musik und Salsa auf den Straßen – Havanna ist pure Karibik.",
+url:"https://de.wikipedia.org/wiki/Havanna",
+image:"https://images.unsplash.com/900x500/?havana,cuba"
+},
+
+{
+title:"Natur & Wandern – Azoren",
+text:"Vulkankrater, grüne Täler und spektakuläre Atlantikblicke machen die Azoren zu einem Traumziel.",
+url:"https://de.wikipedia.org/wiki/Azoren",
+image:"https://images.unsplash.com/900x500/?azores,landscape"
+},
+
+{
+title:"Segeln – Altmühlsee",
+text:"Kleines fränkisches Segelrevier mit viel Natur im Altmühltal.",
+url:"https://de.wikipedia.org/wiki/Altm%C3%BChlsee",
+image:"https://images.unsplash.com/900x500/?altmuhlsee,lake"
+}
+
+]
