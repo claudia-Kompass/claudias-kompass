453d1f8ba5926c7a69fe0f5c9324ee6a02cef274
claudia-Kompass <claudia.foerstner@googlemail.com>
2026-03-15 14:16:56 +0100

    Update travel.js

--- api/data/travel.js
+++ api/data/travel.js
@@ -4,70 +4,210 @@ module.exports = [
 title:"Azoren – Portugal",
 text:"Vulkankrater, heiße Quellen und spektakuläre Atlantikblicke.",
 url:"https://www.visitazores.com",
-image:"https://upload.wikimedia.org/wikipedia/commons/6/65/Lagoa_do_Fogo_Azores.jpg"
-},
-
-{
-title:"Havanna – Kuba",
-text:"Koloniale Altstadt, Karibikflair und Salsa auf den Straßen.",
-url:"https://www.lonelyplanet.com/cuba/havana",
-image:"https://upload.wikimedia.org/wikipedia/commons/0/0b/Havana_Cuba.jpg"
-},
-
-{
-title:"Sardinien – Italien",
-text:"Kristallklares Wasser und Segelreviere im Maddalena-Archipel.",
-url:"https://www.sardegnaturismo.it",
-image:"https://upload.wikimedia.org/wikipedia/commons/e/e1/La_Maddalena_archipelago.jpg"
+image:"https://upload.wikimedia.org/wikipedia/commons/3/3f/Azores_Pico.jpg"
 },
 
 {
 title:"Kyoto – Japan",
 text:"Tempel, Gärten und traditionelle Kultur im Herzen Japans.",
-url:"https://kyoto.travel",
-image:"https://upload.wikimedia.org/wikipedia/commons/6/6e/Kinkaku-ji_Kyoto.jpg"
-},
-
-{
-title:"Kapstadt – Südafrika",
-text:"Tafelberg, Atlantik und eine der schönsten Küsten der Welt.",
-url:"https://www.capetown.travel",
-image:"https://upload.wikimedia.org/wikipedia/commons/a/af/Table_Mountain_from_Bloubergstrand.jpg"
-},
-
-{
-title:"Vancouver – Kanada",
-text:"Meer, Berge und Outdoor-Abenteuer direkt vor der Stadt.",
-url:"https://www.tourismvancouver.com",
-image:"https://upload.wikimedia.org/wikipedia/commons/c/cb/Vancouver_skyline_from_Stanley_Park.jpg"
+url:"https://www.japan.travel/en/destinations/kansai/kyoto/",
+image:"https://upload.wikimedia.org/wikipedia/commons/6/6e/Kiyomizu-dera_Kyoto.jpg"
 },
 
 {
 title:"Patagonien – Chile & Argentinien",
 text:"Gletscher, Wind und endlose Landschaften am Ende der Welt.",
 url:"https://www.nationalgeographic.de/reise/patagonien",
-image:"https://images.unsplash.com/photo-1501785888041-af3ef285b470"
+image:"https://upload.wikimedia.org/wikipedia/commons/0/0c/Torres_del_Paine.jpg"
 },
 
 {
 title:"Madeira – Portugal",
 text:"Blumeninsel im Atlantik mit spektakulären Levadas.",
-url:"https://visitmadeira.com",
-image:"https://upload.wikimedia.org/wikipedia/commons/3/33/Pico_do_Areeiro_Madeira.jpg"
+url:"https://www.visitmadeira.com",
+image:"https://upload.wikimedia.org/wikipedia/commons/3/3b/Madeira_Pico_Ruivo.jpg"
 },
 
 {
-title:"Neuseeland – Südinsel",
-text:"Fjorde, Berge und unberührte Natur im Pazifik.",
-url:"https://www.newzealand.com",
-image:"https://upload.wikimedia.org/wikipedia/commons/5/56/Milford_Sound.jpg"
+title:"Havanna – Kuba",
+text:"Koloniale Architektur, Musik und karibische Lebensfreude.",
+url:"https://www.lonelyplanet.com/cuba/havana",
+image:"https://upload.wikimedia.org/wikipedia/commons/1/1c/Havana_Capitolio.jpg"
 },
 
 {
-title:"Altmühlsee – Deutschland",
-text:"Segeln, Radfahren und Natur im Fränkischen Seenland.",
+title:"Kapstadt – Südafrika",
+text:"Tafelberg, Atlantik und eine der schönsten Städte der Welt.",
+url:"https://www.capetown.travel",
+image:"https://upload.wikimedia.org/wikipedia/commons/1/1c/Table_Mountain_from_Bloubergstrand.jpg"
+},
+
+{
+title:"Lofoten – Norwegen",
+text:"Dramatische Berge, Fjorde und Nordlichter.",
+url:"https://www.visitnorway.com/places-to-go/northern-norway/the-lofoten-islands/",
+image:"https://upload.wikimedia.org/wikipedia/commons/e/e5/Lofoten_Reine.jpg"
+},
+
+{
+title:"Galapagos – Ecuador",
+text:"Einzigartige Tierwelt und Vulkaninseln im Pazifik.",
+url:"https://www.galapagos.org",
+image:"https://upload.wikimedia.org/wikipedia/commons/3/30/Galapagos_Islands.jpg"
+},
+
+{
+title:"Santorini – Griechenland",
+text:"Weiße Häuser über dem Ägäischen Meer.",
+url:"https://www.visitgreece.gr/islands/cyclades/santorini/",
+image:"https://upload.wikimedia.org/wikipedia/commons/c/c5/Santorini_Oia.jpg"
+},
+
+{
+title:"Banff – Kanada",
+text:"Türkisfarbene Seen und die Rocky Mountains.",
+url:"https://www.banfflakelouise.com",
+image:"https://upload.wikimedia.org/wikipedia/commons/2/2c/Lake_Louise_Banff.jpg"
+},
+
+{
+title:"Iceland Ring Road",
+text:"Vulkane, Gletscher und Wasserfälle rund um Island.",
+url:"https://visiticeland.com",
+image:"https://upload.wikimedia.org/wikipedia/commons/9/9f/Skogafoss.jpg"
+},
+
+{
+title:"Raja Ampat – Indonesien",
+text:"Eines der besten Tauchgebiete der Welt.",
+url:"https://www.indonesia.travel",
+image:"https://upload.wikimedia.org/wikipedia/commons/2/21/Raja_Ampat_Islands.jpg"
+},
+
+{
+title:"Petra – Jordanien",
+text:"Antike Felsenstadt in der Wüste.",
+url:"https://visitpetra.jo",
+image:"https://upload.wikimedia.org/wikipedia/commons/e/e3/Petra_Jordan.jpg"
+},
+
+{
+title:"Great Barrier Reef – Australien",
+text:"Größtes Korallenriff der Erde.",
+url:"https://www.australia.com",
+image:"https://upload.wikimedia.org/wikipedia/commons/4/4f/Great_Barrier_Reef.jpg"
+},
+
+{
+title:"Machu Picchu – Peru",
+text:"Inkastadt hoch in den Anden.",
+url:"https://www.peru.travel",
+image:"https://upload.wikimedia.org/wikipedia/commons/e/eb/Machu_Picchu.jpg"
+},
+
+{
+title:"Amalfi Küste – Italien",
+text:"Dramatische Küstenstraße und mediterrane Dörfer.",
+url:"https://www.italia.it",
+image:"https://upload.wikimedia.org/wikipedia/commons/6/6f/Amalfi_Coast.jpg"
+},
+
+{
+title:"Vancouver – Kanada",
+text:"Stadt zwischen Ozean und Bergen.",
+url:"https://www.tourismvancouver.com",
+image:"https://upload.wikimedia.org/wikipedia/commons/5/5d/Vancouver_skyline.jpg"
+},
+
+{
+title:"Salar de Uyuni – Bolivien",
+text:"Größte Salzwüste der Welt.",
+url:"https://www.bolivia.travel",
+image:"https://upload.wikimedia.org/wikipedia/commons/8/89/Salar_de_Uyuni.jpg"
+},
+
+{
+title:"Serengeti – Tansania",
+text:"Safari und große Tiermigration.",
+url:"https://www.tanzaniatourism.go.tz",
+image:"https://upload.wikimedia.org/wikipedia/commons/0/03/Serengeti_Landscape.jpg"
+},
+
+{
+title:"New York – USA",
+text:"Die Stadt die niemals schläft.",
+url:"https://www.nycgo.com",
+image:"https://upload.wikimedia.org/wikipedia/commons/4/47/New_york_times_square.jpg"
+},
+
+{
+title:"Tokyo – Japan",
+text:"Moderne Megacity und traditionelle Tempel.",
+url:"https://www.gotokyo.org",
+image:"https://upload.wikimedia.org/wikipedia/commons/1/13/Tokyo_Skyline.jpg"
+},
+
+{
+title:"Sydney – Australien",
+text:"Opernhaus und spektakuläre Hafenlage.",
+url:"https://www.sydney.com",
+image:"https://upload.wikimedia.org/wikipedia/commons/4/40/Sydney_Opera_House.jpg"
+},
+
+{
+title:"Bali – Indonesien",
+text:"Tempel, Reisterrassen und tropische Strände.",
+url:"https://www.indonesia.travel",
+image:"https://upload.wikimedia.org/wikipedia/commons/9/9f/Bali_Temple.jpg"
+},
+
+{
+title:"Seychellen",
+text:"Traumstrände und Granitfelsen im Indischen Ozean.",
+url:"https://www.seychelles.travel",
+image:"https://upload.wikimedia.org/wikipedia/commons/2/29/Anse_Source_dArgent.jpg"
+},
+
+{
+title:"Maldiven",
+text:"Koralleninseln und türkisfarbene Lagunen.",
+url:"https://visitmaldives.com",
+image:"https://upload.wikimedia.org/wikipedia/commons/f/f2/Maldives_islands.jpg"
+},
+
+{
+title:"Dolomiten – Italien",
+text:"Spektakuläre Berglandschaft und Wanderparadies.",
+url:"https://www.dolomiti.org",
+image:"https://upload.wikimedia.org/wikipedia/commons/7/77/Dolomites_mountains.jpg"
+},
+
+{
+title:"Altmühlsee – Bayern",
+text:"Segeln, Radfahren und Naturpark Altmühltal.",
 url:"https://www.fraenkisches-seenland.de",
-image:"https://upload.wikimedia.org/wikipedia/commons/6/6e/Altmuehlsee_Staudamm.jpg"
+image:"https://upload.wikimedia.org/wikipedia/commons/4/4a/Altmuehlsee.jpg"
+},
+
+{
+title:"Dubrovnik – Kroatien",
+text:"Mittelalterliche Stadtmauern an der Adria.",
+url:"https://www.visitdubrovnik.hr",
+image:"https://upload.wikimedia.org/wikipedia/commons/6/6d/Dubrovnik_old_town.jpg"
+},
+
+{
+title:"Prag – Tschechien",
+text:"Goldene Stadt mit barocker Architektur.",
+url:"https://www.prague.eu",
+image:"https://upload.wikimedia.org/wikipedia/commons/a/a7/Prague_Castle.jpg"
+},
+
+{
+title:"Barcelona – Spanien",
+text:"Gaudí Architektur und mediterranes Lebensgefühl.",
+url:"https://www.barcelonaturisme.com",
+image:"https://upload.wikimedia.org/wikipedia/commons/6/6e/Sagrada_Familia.jpg"
 }
 
 ]
