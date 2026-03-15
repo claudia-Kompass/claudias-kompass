b1e64d4b67ca12cd2bd76a5a983c46fba97c502c
claudia-Kompass <claudia.foerstner@googlemail.com>
2026-03-15 09:05:35 +0100

    Update travel.js

--- api/data/travel.js
+++ api/data/travel.js
@@ -2,30 +2,72 @@ module.exports = [
 
 {
 title:"Segeln – Sardinien",
-text:"Kristallklares Wasser und der Maddalena-Archipel machen Sardinien zu einem der schönsten Segelreviere im Mittelmeer.",
-url:"https://de.wikipedia.org/wiki/Sardinien",
-image:"https://images.unsplash.com/900x500/?sailing,sardinia"
-},
-
-{
-title:"Salsa & Kultur – Havanna",
-text:"Koloniale Architektur, kubanische Musik und Salsa auf den Straßen – Havanna ist pure Karibik.",
-url:"https://de.wikipedia.org/wiki/Havanna",
-image:"https://images.unsplash.com/900x500/?havana,cuba"
+text:"Der Maddalena-Archipel im Norden Sardiniens gehört zu den schönsten Segelrevieren im Mittelmeer. Türkisfarbenes Wasser, kurze Distanzen zwischen den Inseln und geschützte Buchten machen das Gebiet ideal für entspannte Törns.",
+url:"https://www.sardegnaturismo.it/en/explore/la-maddalena-archipelago",
+image:"https://images.unsplash.com/900x500/?sardinia,sailing"
 },
 
 {
 title:"Natur & Wandern – Azoren",
-text:"Vulkankrater, grüne Täler und spektakuläre Atlantikblicke machen die Azoren zu einem Traumziel.",
-url:"https://de.wikipedia.org/wiki/Azoren",
+text:"Vulkanlandschaften, Kraterseen und heiße Quellen prägen die Azoren. Besonders die Insel São Miguel bietet spektakuläre Wanderungen entlang der Sete-Cidades-Krater.",
+url:"https://www.visitazores.com/de",
 image:"https://images.unsplash.com/900x500/?azores,landscape"
 },
 
 {
+title:"Salsa & Kultur – Havanna",
+text:"Koloniale Architektur, Oldtimer und kubanische Musik prägen das Leben in Havanna. In den Casas de la Música tanzt man Salsa bis tief in die Nacht.",
+url:"https://www.cuba.travel/de/destinations/havana",
+image:"https://images.unsplash.com/900x500/?havana,cuba"
+},
+
+{
+title:"SUP & Berge – Gardasee",
+text:"Der Gardasee verbindet mediterranes Klima mit alpiner Kulisse. Perfekt für Stand-Up-Paddling am Morgen und kleine Wanderungen in den umliegenden Bergen.",
+url:"https://www.visitgarda.com/de",
+image:"https://images.unsplash.com/900x500/?lake,garda"
+},
+
+{
+title:"Levadas & Atlantik – Madeira",
+text:"Madeira ist berühmt für seine Levadas – historische Wasserkanäle mit spektakulären Wanderwegen durch Lorbeerwälder und entlang steiler Atlantikküsten.",
+url:"https://www.visitmadeira.com/de",
+image:"https://images.unsplash.com/900x500/?madeira,landscape"
+},
+
+{
+title:"Segeln & Inseln – Kroatische Küste",
+text:"Über tausend Inseln machen Kroatien zu einem der beliebtesten Segelreviere Europas. Besonders die Region um Hvar und Vis bietet kristallklares Wasser und charmante Hafenstädte.",
+url:"https://croatia.hr/de-de/erlebnisse/segeln",
+image:"https://images.unsplash.com/900x500/?croatia,sailing"
+},
+
+{
+title:"Fjorde & Natur – Norwegen",
+text:"Die norwegischen Fjorde zählen zu den spektakulärsten Landschaften Europas. Kajaktouren und Wanderungen bieten einzigartige Perspektiven auf steile Felswände und Wasserfälle.",
+url:"https://www.visitnorway.com",
+image:"https://images.unsplash.com/900x500/?norway,fjord"
+},
+
+{
+title:"Atlantik & Surf – Portugal",
+text:"Die Küste rund um Ericeira und Peniche ist ein Paradies für Surfer und Atlantikliebhaber. Dramatische Klippen und kleine Fischerdörfer prägen die Region.",
+url:"https://www.visitportugal.com/de",
+image:"https://images.unsplash.com/900x500/?portugal,coast"
+},
+
+{
 title:"Segeln – Altmühlsee",
-text:"Kleines fränkisches Segelrevier mit viel Natur im Altmühltal.",
-url:"https://www.fraenkisches-seenland.de/freizeit/a-z/personenschifffahrt_auf_dem_al-32/
+text:"Der Altmühlsee im Fränkischen Seenland ist ein ruhiges Segelrevier mit viel Natur. Ideal für entspannte Tage auf dem Wasser oder eine Radtour rund um den See.",
+url:"https://www.fraenkisches-seenland.de/seen/altmuehlsee/",
 image:"https://images.unsplash.com/900x500/?altmuhlsee,lake"
+},
+
+{
+title:"Korallen & Tauchen – Rotes Meer",
+text:"Das Rote Meer gehört zu den besten Tauchrevieren der Welt. Farbenreiche Korallenriffe und klares Wasser machen Orte wie Hurghada oder Marsa Alam besonders beliebt.",
+url:"https://www.redsea.gov.eg",
+image:"https://images.unsplash.com/900x500/?red-sea,diving"
 }
 
 ]
