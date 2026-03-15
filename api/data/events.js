112674a787048cc710f169f2e71e3db2351425f6
claudia-Kompass <claudia.foerstner@googlemail.com>
2026-03-15 16:05:58 +0100

    Update events.js

--- api/data/events.js
+++ api/data/events.js
@@ -16,6 +16,17 @@ url:"https://www.hohenloher-verbrauchermesse.de"
 },
 
 {
+title:"CMT – Urlaubsmesse",
+city:"Stuttgart",
+location:"Messe Stuttgart",
+address:"Messepiazza 1, 70629 Stuttgart",
+month:1,
+day:17,
+time:"10:00–18:00",
+url:"https://www.messe-stuttgart.de/cmt"
+},
+
+{
 title:"Freizeit Messe Nürnberg",
 city:"Nürnberg",
 location:"Messezentrum Nürnberg",
@@ -37,28 +48,6 @@ time:"10:00–18:00",
 url:"https://www.consumenta.de"
 },
 
-{
-title:"CMT – Urlaubsmesse",
-city:"Stuttgart",
-location:"Messe Stuttgart",
-address:"Messepiazza 1, 70629 Stuttgart",
-month:1,
-day:17,
-time:"10:00–18:00",
-url:"https://www.messe-stuttgart.de/cmt"
-},
-
-{
-title:"Genussmesse Heilbronn",
-city:"Heilbronn",
-location:"redblue Messehalle",
-address:"Wannenäckerstr 50, 74078 Heilbronn",
-month:3,
-day:7,
-time:"10:00–18:00",
-url:"https://redblue.de"
-},
-
 /* ======================
    SCHWÄBISCH HALL
 ====================== */
@@ -69,25 +58,9 @@ city:"Schwäbisch Hall",
 location:"Altstadt",
 address:"Marktplatz 1, 74523 Schwäbisch Hall",
 month:4,
-day:6
-},
-
-{
-title:"Jakobimarkt",
-city:"Schwäbisch Hall",
-location:"Altstadt",
-address:"Marktplatz 1, 74523 Schwäbisch Hall",
-month:7,
-day:26
-},
-
-{
-title:"Sommernachtsfest",
-city:"Schwäbisch Hall",
-location:"Altstadt",
-address:"Marktplatz 1, 74523 Schwäbisch Hall",
-month:8,
-day:10
+day:6,
+time:"11:00–18:00",
+url:"https://www.schwaebischhall.de"
 },
 
 {
@@ -96,7 +69,31 @@ city:"Schwäbisch Hall",
 location:"Altstadt",
 address:"Marktplatz 1, 74523 Schwäbisch Hall",
 month:6,
-day:9
+day:9,
+time:"11:00–18:00",
+url:"https://www.schwaebischhall.de"
+},
+
+{
+title:"Jakobimarkt",
+city:"Schwäbisch Hall",
+location:"Altstadt",
+address:"Marktplatz 1, 74523 Schwäbisch Hall",
+month:7,
+day:26,
+time:"10:00–18:00",
+url:"https://www.schwaebischhall.de"
+},
+
+{
+title:"Sommernachtsfest",
+city:"Schwäbisch Hall",
+location:"Altstadt",
+address:"Marktplatz 1, 74523 Schwäbisch Hall",
+month:8,
+day:10,
+time:"18:00–24:00",
+url:"https://www.schwaebischhall.de"
 },
 
 {
@@ -105,7 +102,9 @@ city:"Schwäbisch Hall",
 location:"Marktplatz",
 address:"Marktplatz 1, 74523 Schwäbisch Hall",
 month:12,
-day:5
+day:5,
+time:"11:00–20:00",
+url:"https://www.schwaebischhall.de"
 },
 
 /* ======================
@@ -113,34 +112,103 @@ day:5
 ====================== */
 
 {
-title:"Crailsheimer Volksfest",
-city:"Crailsheim",
-location:"Volksfestplatz",
-address:"Beuerlbacher Str, 74564 Crailsheim",
-month:9,
-day:19
-},
-
-{
 title:"Street Food Festival",
 city:"Crailsheim",
 location:"Innenstadt",
 address:"Marktplatz 1, 74564 Crailsheim",
 month:6,
-day:15
+day:15,
+time:"11:00–22:00",
+url:"https://www.crailsheim.de"
+},
+
+{
+title:"Crailsheimer Volksfest",
+city:"Crailsheim",
+location:"Volksfestplatz",
+address:"Beuerlbacher Str, 74564 Crailsheim",
+month:9,
+day:19,
+time:"11:00–23:00",
+url:"https://www.crailsheim.de"
 },
 
 /* ======================
-   GAILDORF
+   ÖHRINGEN
 ====================== */
 
 {
-title:"Gaildorfer Pferdemarkt",
-city:"Gaildorf",
+title:"Öhringer Frühlingsmarkt",
+city:"Öhringen",
 location:"Innenstadt",
-address:"Marktplatz, 74405 Gaildorf",
-month:2,
-day:10
+address:"Marktplatz 1, 74613 Öhringen",
+month:4,
+day:13,
+time:"11:00–18:00",
+url:"https://www.oehringen.de"
+},
+
+{
+title:"Hohenloher Weindorf",
+city:"Öhringen",
+location:"Marktplatz",
+address:"Marktplatz 1, 74613 Öhringen",
+month:8,
+day:22,
+time:"17:00–23:00",
+url:"https://www.oehringen.de"
+},
+
+{
+title:"Öhringer Herbstmarkt",
+city:"Öhringen",
+location:"Innenstadt",
+address:"Marktplatz 1, 74613 Öhringen",
+month:10,
+day:12,
+time:"11:00–18:00",
+url:"https://www.oehringen.de"
+},
+
+/* ======================
+   ANSBACH
+====================== */
+
+{
+title:"Frühlingsmarkt Ansbach",
+city:"Ansbach",
+location:"Innenstadt",
+address:"Johann-Sebastian-Bach-Platz, 91522 Ansbach",
+month:4,
+day:5,
+time:"11:00–18:00",
+url:"https://www.ansbach.de"
+},
+
+{
+title:"Herbstmarkt Ansbach",
+city:"Ansbach",
+location:"Innenstadt",
+address:"Johann-Sebastian-Bach-Platz, 91522 Ansbach",
+month:10,
+day:12,
+time:"11:00–18:00",
+url:"https://www.ansbach.de"
+},
+
+/* ======================
+   FEUCHTWANGEN
+====================== */
+
+{
+title:"Altstadtfest Feuchtwangen",
+city:"Feuchtwangen",
+location:"Altstadt",
+address:"Marktplatz 1, 91555 Feuchtwangen",
+month:7,
+day:18,
+time:"17:00–23:00",
+url:"https://www.feuchtwangen.de"
 },
 
 /* ======================
@@ -153,7 +221,9 @@ city:"Dinkelsbühl",
 location:"Altstadt",
 address:"Marktplatz, 91550 Dinkelsbühl",
 month:7,
-day:20
+day:20,
+time:"10:00–23:00",
+url:"https://www.kinderzeche.de"
 },
 
 {
@@ -162,7 +232,61 @@ city:"Dinkelsbühl",
 location:"Altstadt",
 address:"Marktplatz, 91550 Dinkelsbühl",
 month:12,
-day:10
+day:10,
+time:"11:00–20:00",
+url:"https://www.dinkelsbuehl.de"
+},
+
+/* ======================
+   BROMBACHSEE
+====================== */
+
+{
+title:"Seenlandmarkt Brombachsee",
+city:"Pleinfeld",
+location:"Segelhafen Ramsberg",
+address:"Am Segelhafen 1, 91785 Pleinfeld",
+month:5,
+day:18,
+time:"11:00–18:00",
+url:"https://www.fraenkisches-seenland.de"
+},
+
+{
+title:"Brombachsee Sommerfest",
+city:"Pleinfeld",
+location:"Großer Brombachsee",
+address:"Am Segelhafen 1, 91785 Pleinfeld",
+month:7,
+day:20,
+time:"12:00–23:00",
+url:"https://www.fraenkisches-seenland.de"
+},
+
+/* ======================
+   ALTMÜHLSEE
+====================== */
+
+{
+title:"Altmühlsee Seefest",
+city:"Gunzenhausen",
+location:"Seezentrum Schlungenhof",
+address:"Seestraße 19, 91710 Gunzenhausen",
+month:7,
+day:12,
+time:"12:00–23:00",
+url:"https://www.fraenkisches-seenland.de"
+},
+
+{
+title:"Altmühlsee Herbstmarkt",
+city:"Gunzenhausen",
+location:"Altstadt",
+address:"Marktplatz 1, 91710 Gunzenhausen",
+month:10,
+day:5,
+time:"11:00–18:00",
+url:"https://www.gunzenhausen.de"
 }
 
 ]
