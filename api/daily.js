61e11022bc5e79b09723035dacc2360ae3242cd0
claudia-Kompass <claudia.foerstner@googlemail.com>
2026-03-20 20:04:05 +0100

    Update daily.js

--- api/daily.js
+++ api/daily.js
@@ -159,10 +159,8 @@ API FETCH
 const results = await Promise.allSettled([
 
 fetchTimeout("https://api.open-meteo.com/v1/forecast?latitude=49.17&longitude=9.92&current=temperature_2m,weathercode&hourly=temperature_2m,weathercode"),
-
-fetch("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,nexo,pax-gold,brent-crude-oil&vs_currencies=usd,eur&include_24hr_change=true&precision=full"),
+fetchTimeout("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,nexo,pax-gold,brent-crude-oil&vs_currencies=usd,eur&include_24hr_change=true"),
 fetchTimeout("https://open.er-api.com/v6/latest/EUR"),
-
 fetchTimeout("https://stooq.com/q/l/?s=dax&i=d"),
 fetchTimeout("https://api.allorigins.win/raw?url=https://www.finanzen.net/rohstoffe/oelpreis"),
 fetchTimeout("https://www.tagesschau.de/xml/rss2/"),
