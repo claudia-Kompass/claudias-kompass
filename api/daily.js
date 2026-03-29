f1a7b66e32e95891ddef4e6df18e1eeace6e9899
claudia-Kompass <claudia.foerstner@googlemail.com>
2026-03-29 20:32:24 +0200

    Update daily.js

--- api/daily.js
+++ api/daily.js
@@ -905,7 +905,7 @@ function enrichEvent(e){
 const rawEvents = (await loadEvents()).map(enrichEvent)
 
 const eventsClean = rawEvents
-.filter(e => true)
+  .filter(e => e.category !== "dance")
   .map(e => ({
     ...e,
     maps: e.maps || buildMaps(e)
