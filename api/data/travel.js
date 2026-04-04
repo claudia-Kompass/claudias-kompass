07793dc8beaed00e323f333f4a62bd2064de80c7
claudia-Kompass <claudia.foerstner@googlemail.com>
2026-04-04 17:45:21 +0200

    Update travel.js

--- api/data/travel.js
+++ api/data/travel.js
@@ -4,210 +4,175 @@ module.exports = [
 title:"Azoren – Portugal",
 text:"Vulkankrater, heiße Quellen und spektakuläre Atlantikblicke.",
 url:"https://www.visitazores.com",
-image:"https://upload.wikimedia.org/wikipedia/commons/3/3f/Azores_Pico.jpg"
+image:"https://images.unsplash.com/photo-1500375592092-40eb2168fd21?auto=format&fit=crop&w=1200&q=60"
 },
 
 {
 title:"Kyoto – Japan",
 text:"Tempel, Gärten und traditionelle Kultur im Herzen Japans.",
 url:"https://www.japan.travel/en/destinations/kansai/kyoto/",
-image:"https://upload.wikimedia.org/wikipedia/commons/6/6e/Kiyomizu-dera_Kyoto.jpg"
+image:"https://images.unsplash.com/photo-1504788363733-507549153474?auto=format&fit=crop&w=1200&q=60"
 },
 
 {
 title:"Patagonien – Chile & Argentinien",
 text:"Gletscher, Wind und endlose Landschaften am Ende der Welt.",
 url:"https://www.nationalgeographic.de/reise/patagonien",
-image:"https://upload.wikimedia.org/wikipedia/commons/0/0c/Torres_del_Paine.jpg"
+image:"https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?auto=format&fit=crop&w=1200&q=60"
 },
 
 {
 title:"Madeira – Portugal",
 text:"Blumeninsel im Atlantik mit spektakulären Levadas.",
 url:"https://www.visitmadeira.com",
-image:"https://upload.wikimedia.org/wikipedia/commons/3/3b/Madeira_Pico_Ruivo.jpg"
+image:"https://images.unsplash.com/photo-1501594907352-04cda38ebc29?auto=format&fit=crop&w=1200&q=60"
 },
 
 {
 title:"Havanna – Kuba",
 text:"Koloniale Architektur, Musik und karibische Lebensfreude.",
 url:"https://www.lonelyplanet.com/cuba/havana",
-image:"https://upload.wikimedia.org/wikipedia/commons/1/1c/Havana_Capitolio.jpg"
+image:"https://images.unsplash.com/photo-1520975922284-9b7b6f6d1c0f?auto=format&fit=crop&w=1200&q=60"
 },
 
 {
 title:"Kapstadt – Südafrika",
 text:"Tafelberg, Atlantik und eine der schönsten Städte der Welt.",
 url:"https://www.capetown.travel",
-image:"https://upload.wikimedia.org/wikipedia/commons/1/1c/Table_Mountain_from_Bloubergstrand.jpg"
+image:"https://images.unsplash.com/photo-1501594907352-04cda38ebc29?auto=format&fit=crop&w=1200&q=60"
 },
 
 {
 title:"Lofoten – Norwegen",
 text:"Dramatische Berge, Fjorde und Nordlichter.",
-url:"https://www.visitnorway.com/places-to-go/northern-norway/the-lofoten-islands/",
-image:"https://upload.wikimedia.org/wikipedia/commons/e/e5/Lofoten_Reine.jpg"
+url:"https://www.visitnorway.com",
+image:"https://images.unsplash.com/photo-1500534314209-a25ddb2bd429?auto=format&fit=crop&w=1200&q=60"
 },
 
 {
 title:"Galapagos – Ecuador",
 text:"Einzigartige Tierwelt und Vulkaninseln im Pazifik.",
 url:"https://www.galapagos.org",
-image:"https://upload.wikimedia.org/wikipedia/commons/3/30/Galapagos_Islands.jpg"
+image:"https://images.unsplash.com/photo-1500534314209-a25ddb2bd429?auto=format&fit=crop&w=1200&q=60"
 },
 
 {
 title:"Santorini – Griechenland",
 text:"Weiße Häuser über dem Ägäischen Meer.",
-url:"https://www.visitgreece.gr/islands/cyclades/santorini/",
-image:"https://upload.wikimedia.org/wikipedia/commons/c/c5/Santorini_Oia.jpg"
+url:"https://www.visitgreece.gr",
+image:"https://images.unsplash.com/photo-1507525428034-b723cf961d3e?auto=format&fit=crop&w=1200&q=60"
 },
 
 {
 title:"Banff – Kanada",
 text:"Türkisfarbene Seen und die Rocky Mountains.",
 url:"https://www.banfflakelouise.com",
-image:"https://upload.wikimedia.org/wikipedia/commons/2/2c/Lake_Louise_Banff.jpg"
+image:"https://images.unsplash.com/photo-1501785888041-af3ef285b470?auto=format&fit=crop&w=1200&q=60"
 },
 
 {
 title:"Iceland Ring Road",
 text:"Vulkane, Gletscher und Wasserfälle rund um Island.",
 url:"https://visiticeland.com",
-image:"https://upload.wikimedia.org/wikipedia/commons/9/9f/Skogafoss.jpg"
+image:"https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?auto=format&fit=crop&w=1200&q=60"
 },
 
 {
 title:"Raja Ampat – Indonesien",
 text:"Eines der besten Tauchgebiete der Welt.",
 url:"https://www.indonesia.travel",
-image:"https://upload.wikimedia.org/wikipedia/commons/2/21/Raja_Ampat_Islands.jpg"
+image:"https://images.unsplash.com/photo-1507525428034-b723cf961d3e?auto=format&fit=crop&w=1200&q=60"
 },
 
 {
 title:"Petra – Jordanien",
 text:"Antike Felsenstadt in der Wüste.",
 url:"https://visitpetra.jo",
-image:"https://upload.wikimedia.org/wikipedia/commons/e/e3/Petra_Jordan.jpg"
+image:"https://images.unsplash.com/photo-1500534314209-a25ddb2bd429?auto=format&fit=crop&w=1200&q=60"
 },
 
 {
 title:"Great Barrier Reef – Australien",
 text:"Größtes Korallenriff der Erde.",
 url:"https://www.australia.com",
-image:"https://upload.wikimedia.org/wikipedia/commons/4/4f/Great_Barrier_Reef.jpg"
+image:"https://images.unsplash.com/photo-1507525428034-b723cf961d3e?auto=format&fit=crop&w=1200&q=60"
 },
 
 {
 title:"Machu Picchu – Peru",
 text:"Inkastadt hoch in den Anden.",
 url:"https://www.peru.travel",
-image:"https://upload.wikimedia.org/wikipedia/commons/e/eb/Machu_Picchu.jpg"
+image:"https://images.unsplash.com/photo-1505678261036-a3fcc5e884ee?auto=format&fit=crop&w=1200&q=60"
 },
 
 {
 title:"Amalfi Küste – Italien",
 text:"Dramatische Küstenstraße und mediterrane Dörfer.",
 url:"https://www.italia.it",
-image:"https://upload.wikimedia.org/wikipedia/commons/6/6f/Amalfi_Coast.jpg"
+image:"https://images.unsplash.com/photo-1501594907352-04cda38ebc29?auto=format&fit=crop&w=1200&q=60"
 },
 
 {
 title:"Vancouver – Kanada",
 text:"Stadt zwischen Ozean und Bergen.",
 url:"https://www.tourismvancouver.com",
-image:"https://upload.wikimedia.org/wikipedia/commons/5/5d/Vancouver_skyline.jpg"
+image:"https://images.unsplash.com/photo-1505761671935-60b3a7427bad?auto=format&fit=crop&w=1200&q=60"
 },
 
 {
 title:"Salar de Uyuni – Bolivien",
 text:"Größte Salzwüste der Welt.",
 url:"https://www.bolivia.travel",
-image:"https://upload.wikimedia.org/wikipedia/commons/8/89/Salar_de_Uyuni.jpg"
+image:"https://images.unsplash.com/photo-1500534314209-a25ddb2bd429?auto=format&fit=crop&w=1200&q=60"
 },
 
 {
 title:"Serengeti – Tansania",
 text:"Safari und große Tiermigration.",
 url:"https://www.tanzaniatourism.go.tz",
-image:"https://upload.wikimedia.org/wikipedia/commons/0/03/Serengeti_Landscape.jpg"
+image:"https://images.unsplash.com/photo-1500534314209-a25ddb2bd429?auto=format&fit=crop&w=1200&q=60"
 },
 
 {
 title:"New York – USA",
 text:"Die Stadt die niemals schläft.",
 url:"https://www.nycgo.com",
-image:"https://upload.wikimedia.org/wikipedia/commons/4/47/New_york_times_square.jpg"
+image:"https://images.unsplash.com/photo-1496440737103-cd596325d314?auto=format&fit=crop&w=1200&q=60"
 },
 
 {
 title:"Tokyo – Japan",
 text:"Moderne Megacity und traditionelle Tempel.",
 url:"https://www.gotokyo.org",
-image:"https://upload.wikimedia.org/wikipedia/commons/1/13/Tokyo_Skyline.jpg"
+image:"https://images.unsplash.com/photo-1505067216361-1f0fdfc98e30?auto=format&fit=crop&w=1200&q=60"
 },
 
 {
 title:"Sydney – Australien",
 text:"Opernhaus und spektakuläre Hafenlage.",
 url:"https://www.sydney.com",
-image:"https://upload.wikimedia.org/wikipedia/commons/4/40/Sydney_Opera_House.jpg"
+image:"https://images.unsplash.com/photo-1506973035872-a4f23b7c1b1b?auto=format&fit=crop&w=1200&q=60"
 },
 
 {
 title:"Bali – Indonesien",
 text:"Tempel, Reisterrassen und tropische Strände.",
 url:"https://www.indonesia.travel",
-image:"https://upload.wikimedia.org/wikipedia/commons/9/9f/Bali_Temple.jpg"
+image:"https://images.unsplash.com/photo-1507525428034-b723cf961d3e?auto=format&fit=crop&w=1200&q=60"
 },
 
 {
 title:"Seychellen",
 text:"Traumstrände und Granitfelsen im Indischen Ozean.",
 url:"https://www.seychelles.travel",
-image:"https://upload.wikimedia.org/wikipedia/commons/2/29/Anse_Source_dArgent.jpg"
+image:"https://images.unsplash.com/photo-1507525428034-b723cf961d3e?auto=format&fit=crop&w=1200&q=60"
 },
 
 {
 title:"Maldiven",
 text:"Koralleninseln und türkisfarbene Lagunen.",
 url:"https://visitmaldives.com",
-image:"https://upload.wikimedia.org/wikipedia/commons/f/f2/Maldives_islands.jpg"
-},
-
-{
-title:"Dolomiten – Italien",
-text:"Spektakuläre Berglandschaft und Wanderparadies.",
-url:"https://www.dolomiti.org",
-image:"https://upload.wikimedia.org/wikipedia/commons/7/77/Dolomites_mountains.jpg"
-},
-
-{
-title:"Altmühlsee – Bayern",
-text:"Segeln, Radfahren und Naturpark Altmühltal.",
-url:"https://www.fraenkisches-seenland.de",
-image:"https://upload.wikimedia.org/wikipedia/commons/4/4a/Altmuehlsee.jpg"
-},
-
-{
-title:"Dubrovnik – Kroatien",
-text:"Mittelalterliche Stadtmauern an der Adria.",
-url:"https://www.visitdubrovnik.hr",
-image:"https://upload.wikimedia.org/wikipedia/commons/6/6d/Dubrovnik_old_town.jpg"
-},
-
-{
-title:"Prag – Tschechien",
-text:"Goldene Stadt mit barocker Architektur.",
-url:"https://www.prague.eu",
-image:"https://upload.wikimedia.org/wikipedia/commons/a/a7/Prague_Castle.jpg"
-},
-
-{
-title:"Barcelona – Spanien",
-text:"Gaudí Architektur und mediterranes Lebensgefühl.",
-url:"https://www.barcelonaturisme.com",
-image:"https://upload.wikimedia.org/wikipedia/commons/6/6e/Sagrada_Familia.jpg"
+image:"https://images.unsplash.com/photo-1507525428034-b723cf961d3e?auto=format&fit=crop&w=1200&q=60"
 }
 
 ]
