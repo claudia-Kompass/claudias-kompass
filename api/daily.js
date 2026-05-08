7360dca64c6eac2c166b4014df58ee0ba5d1b2c0
claudia-Kompass <claudia.foerstner@googlemail.com>
2026-05-09 00:05:16 +0200

    Update city check to include 'Schwäbisch Hall'

--- api/daily.js
+++ api/daily.js
@@ -1322,7 +1322,7 @@ console.log("PARKING EVENTS", all)
     const city = (e.city || "").toLowerCase()
 
     return (
-      city.includes(" hall") &&
+      city.includes("Schwäbisch Hall") &&
       blockingKeywords.some(k => title.includes(k))
     )
   })
